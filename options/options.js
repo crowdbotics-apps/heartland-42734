6fca3fe00d069c1690012c3478c79ea39a1ebb00
Crowdbotics <team@crowdbotics.com>
2023-08-09 00:00:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-waterfall-42734",
+  url: "https://late-waterfall-42734.botics.co",
+  api: "https://late-waterfall-42734.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
